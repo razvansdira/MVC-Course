f9ff2bcd2d26975524f0dba13e24d69ad73b94fa
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-21 23:10:46 +0200

    Course 6 Exercises 8 and 9
    Course 6, Exercises 8 and 9.

---
+++ ActionResultController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+//using System.Web.Mvc.FileResult;
+
+namespace ProjectManagement.Controllers
+{
+    public class ActionResultController:Controller
+    {
+        public ActionResult RedirectPermanent()
+        {
+            return Content("This message is sent from the Index action!”;");
+        }
+
+        public ActionResult RedirectToActionInTheSameController()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RedirectToActionInHomeController()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RedirectToRoute()
+        {
+            return RedirectToRoute("About");
+        }
+
+        public ActionResult ReturnFile()
+        {
+            return Content("Site.css");
+        }
+    }
+}
